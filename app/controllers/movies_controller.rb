8a444704537f3c336c85d26d78f8669ebfec55fb
alee115 <alee115@uncc.edu>
2017-10-24 19:40:24 +0000

    revert changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,28 +11,17 @@ class MoviesController < ApplicationController
   end
 
   def index
-   if(!params.has_key?(:sort) && !params.has_key?(:ratings))
-      if(session.has_key?(:sort) || session.has_key?(:ratings))
-        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
-      end
+    @movies = Movie.all
+    @checked_ratings = check
+    @checked_ratings.each do |rating|
+      params[rating] = true
     end
-    @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
-    @all_ratings = Movie.all_ratings.keys
-    @ratings = params[:ratings]
-    if(@ratings != nil)
-      ratings = @ratings.keys
-      session[:ratings] = @ratings
+    
+    if params[:sort]
+      @movies = Movie.order(params[:sort])
     else
-      if(!params.has_key?(:commit) && !params.has_key?(:sort))
-        ratings = Movie.all_ratings.keys
-        session[:ratings] = Movie.all_ratings
-      else
-        ratings = session[:ratings].keys
-      end
+      @movies = Movie.where(:rating => @checked_ratings)
     end
-    @movies = Movie.order(@sort).find_all_by_rating(ratings)
-    @mark  = ratings
-
   end
 
   def new
